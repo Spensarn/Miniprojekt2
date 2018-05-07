3e9f0b65624034850918551930b45b850be53207
Erik <erik.lidbeck@hotmail.se>
2018-05-07 19:35:01 +0200

    Ändrade main() och readFile(File a)
    main() skapar en hashtabell för nyckelord. readFile(File a) skannar 'a' rad för rad och gör string[] med split() som innehåller ord utan sytax. Orden ska sedan läggas i en hashtabell.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 //Metoder: readFiles(File a, File b), compareHash(HashTableClass a, HashTableClass b), print()
+
 public class Main {
 	public static void main(String[] args) {
+		HashTableClass keyWords = new HashTableClass();
+		try {
+			Scanner scan = new Scanner(new File("KeyWords.txt"));
+			while(scan.hasNext()){
+				String keyword = scan.next();
+				keyWords.add(keyword);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 	}
-
-	public static void readFiles(File a, File b){
-		
+	//Scannar filrader och splita raden vid java syntax
+	public static HashTableClass readFile(File a){
+		HashTableClass theHash = new HashTableClass();
+		try {
+			@SuppressWarnings("resource")
+			Scanner scan = new Scanner(a);
+			while(scan.hasNextLine()){
+				String line = scan.nextLine();
+				String[] words = line.split("\\{|\\}|\\(|\\)|\\<|\\>|\\*|\\+|\\-|\\/|\\.|\\%|\\&|\\|");
+				for(String word : words){
+					theHash.add(word);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return theHash;
 	}
 	
 	public static int compareHash(HashTableClass a, HashTableClass b){
