81445265a4af9283083e8959bcea816353e9ea5e
Jacob <jacob.spens@hotmail.se>
2018-05-07 17:43:19 +0200

    Sätter upp alla klasser med kommentarer

--- src/HashTableClass.java
+++ src/HashTableClass.java
@@ -1,4 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Hashtable;
 
+//Spara alla java nyckelord i en hashtabell
+//Spara varje unik identifierare i en annan hashtabell
+
+//Metoder: get(int),size(),put(int, string), hashcode()
 public class HashTableClass {
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+		
+	}
 
+	
+	
+    
+	
 }
