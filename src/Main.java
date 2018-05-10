04aec9b62b97a4435dd004e7a624ce569506e15a
Unknown <erik.lidbeck@hotmail.se>
2018-05-10 16:19:59 +0200

    Gjorde klart readFile(), behöver lite testing
    Lade till isNumeric() och keyWords() i main
    readFile() returnar nu ett hashtable men vi måste kolla att det är rätt.

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.util.Arrays;
-import java.util.Scanner;
 //Metoder: readFiles(File a, File b), compareHash(HashTableClass a, HashTableClass b), print()
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		String[] arrayA = {"hi", "this", "is", "a", "long", "text"};
 		String[] arrayB = {"hi", "this", "is", "a", "much", "longer", "text", "thanks"};
 
@@ -21,56 +18,70 @@ public class Main {
 
 		System.out.println(compareHash(aTable, bTable));
 
-		//		try {
-		//			readFile(new FileReader("Test.txt"));
-		//		} catch (FileNotFoundException e) {
-		//			// TODO Auto-generated catch block
-		//			e.printStackTrace();
-		//		}
+		System.out.println("=============================================");
+		readFile("Miniprojekt2/Test.txt");
 
-		//		HashTableClass keyWords = new HashTableClass();
-		//		try {
-		//			Scanner scan = new Scanner(new File("KeyWords.txt"));
-		//			while(scan.hasNext()){
-		//				String keyword = scan.next();
-		//				keyWords.add(keyword);
-		//			}
-		//		} catch (FileNotFoundException e) {
-		//			// TODO Auto-generated catch block
-		//			e.printStackTrace();
-		//		}
 	}
-	//Scannar filrader och splita raden vid java syntax BYT VOID TILL HASHTABLECLASS N�R DEN �R F�RDING
-	public static void readFile(FileReader a){
-		int wordCount = 0;
+	//Creates HashTableClass of identifiers from file fil(NEEDS MORE TESTING) 
+	public static HashTableClass readFile(String fil){
+		FileReader a = null;
+		try {
+			a = new FileReader(new File(fil));
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		HashTableClass keyWords = keyWords();
+		int idCount = 0;
 		String line = "";
 		try {
-
-			@SuppressWarnings("resource")
 			BufferedReader read = new BufferedReader(a);
 			while(read.ready()){
-
 				line += read.readLine();
-
 			}
-
-			System.out.println(line);
-
-			//String[] words = line.split("\\{|\\}|\\(|\\)|\\<|\\>|\\*|\\+|\\-|\\/|\\.|\\%|\\&|\t|\\s+|\\;|\\|");
-			String[] words = line.split("\\W+");
-			for(String word : words){
-				word.trim();
-				wordCount++;					
-			}
-
-			System.out.println(Arrays.toString(words));
-			System.out.println(wordCount);
+			read.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
 
+		System.out.println(line);
+
+		String[] words = line.split("\\W+"); //Split by symbols (whitespace, {, ], etc.)
+		for(String word : words){//Loop counting how many words are identifiers
+			if(keyWords.contains(word)==null && !isNumeric(word)){
+				idCount++;	
+			}				
+		}
+		String[] idWords = new String [idCount];
+		int i = 0;
+		for(String word : words){//Loop adding identifiers to array
+			if(keyWords.contains(word)==null && !isNumeric(word)){
+				idWords[i] = word;
+				i++;
+			}				
+		}
+		HashTableClass idWordsHash = new HashTableClass(idCount);
+		idWordsHash.add(idWords);
+		return idWordsHash;
+	}
+	//Create a HashTableClass of KeyWords.txt
+	private static HashTableClass keyWords() {
+		String line = "";
+		try {
+			BufferedReader read = new BufferedReader(new FileReader("Miniprojekt2/KeyWords.txt"));
+			while(read.ready()){
+				line += read.readLine();
+			}
+			read.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String[] keyWords = line.split(" ");
+		HashTableClass keyWordHash = new HashTableClass(keyWords.length);
+		keyWordHash.add(keyWords);
+		
+		return keyWordHash;
+	}
 	/**Returns a percentage of how closely the files resemble each other*/
 
 	public static double compareHash(HashTableClass a, HashTableClass b){
@@ -95,6 +106,12 @@ public class Main {
 		double ret = (double) Math.round((counterSameWords / counterTotalWords * 100) * 10) / 10;
 		
 		return ret;
+	}
+	
+	//Check if String is a number
+	public static boolean isNumeric(String str)
+	{
+	  return str.matches("-?\\d+(\\.\\d+)?"); 
 	}
 
 	/**Prints out the percentage*/
